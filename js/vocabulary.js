14f5bf0dd7ce6e6817f08c7cb48c693a939b01ae
Tatiana <djembuka@gmail.com>
2013-06-08 17:38:00 +0400

    New words

--- js/vocabulary.js
+++ js/vocabulary.js
@@ -319,6 +319,155 @@ var vocabulary = {
 		{
 			"item": "Io non sono sposata.",
 			"translation": "Я не замужем."
+		},
+		
+		{
+			"item": "Prego. Per favore.",
+			"translation": "Пожалуйста."
+		},
+		{
+			"item": "Grazie",
+			"translation": "Спасибо."
+		},
+		{
+			"item": "A domani!",
+			"translation": "Ло завтра!"
+		},
+		{
+			"item": "Buon viaggio!",
+			"translation": "Счастливого пути!"
+		},
+		{
+			"item": "Non c'è male.",
+			"translation": "Неплохо."
+		},
+		{
+			"item": "Anch'io",
+			"translation": "Я тоже."
+		},
+		{
+			"item": "Ho un fratello.",
+			"translation": "У меня есть брат."
+		},
+		{
+			"item": "Permesso?",
+			"translation": "Позвольте войти? Можно?"
+		},
+		{
+			"item": "Potrebbe aiutarmi?",
+			"translation": "Не могли бы Вы мне помочь?"
+		},
+		{
+			"item": "Mi dica, per favore...",
+			"translation": "Скажите мне, пожалуйста..."
+		},
+		{
+			"item": "Aspetti un attimo!",
+			"translation": "Подождите минутку!"
+		},
+		{
+			"item": "Entri, per favore!",
+			"translation": "Проходите, пожалуйста."
+		},
+		{
+			"item": "Certo! Sicuro!",
+			"translation": "Конечно же!"
+		},
+		{
+			"item": "D'accordo.",
+			"translation": "Согласен."
+		},
+		{
+			"item": "Con piacere!",
+			"translation": "С удовольствием!"
+		},
+		{
+			"item": "Mille grazie!",
+			"translation": "Большое спасибо!"
+		},
+		{
+			"item": "Scusi, per favore!",
+			"translation": "Извините, пожалуйста!"
+		},
+		{
+			"item": "Scusa!",
+			"translation": "Извини!"
+		},
+		{
+			"item": "Scusi, disturbo?",
+			"translation": "Извините, я не помешаю?"
+		},
+		{
+			"item": "Si, certo.",
+			"translation": "Да, конечно."
+		},
+		{
+			"item": "Ha ragione.",
+			"translation": "Вы правы."
+		},
+		{
+			"item": "Grazie, non c'è bisogno.",
+			"translation": "Спасибо, не нужно."
+		},
+		{
+			"item": "Mi dispiace.",
+			"translation": "Сожалею."
+		},
+		{
+			"item": "Avete torto.",
+			"translation": "Вы не правы."
+		},
+		{
+			"item": "Non voglio.",
+			"translation": "Я не хочу."
+		},
+		{
+			"item": "Ho poco tempo.",
+			"translation": "У меня мало времени."
+		},
+		{
+			"item": "Forse.",
+			"translation": "Может быть."
+		},
+		{
+			"item": "Mi pare.",
+			"translation": "Мне кажется."
+		},
+		{
+			"item": "E possibile.",
+			"translation": "Возможно."
+		},
+		{
+			"item": "Percè no?",
+			"translation": "Почему бы и нет?"
+		}
+		{
+			"item": "Davvero?",
+			"translation": "Неужели?"
+		}
+		{
+			"item": "Come mai?",
+			"translation": "Как же так?"
+		}
+		{
+			"item": "Guarda!",
+			"translation": "Посмотри!"
+		}
+		{
+			"item": "Francamente non mi piace.",
+			"translation": "Честно говоря, мне не нравится."
+		}
+		{
+			"item": "Purtroppo...",
+			"translation": "К сожалению..."
+		}
+		{
+			"item": "Benissimo!",
+			"translation": "Прекрасно!"
+		}
+		{
+			"item": "Che bello!",
+			"translation": "Как красиво!"
 		}
 	],
 	"ελληνικά": [
@@ -349,6 +498,158 @@ var vocabulary = {
 		{
 			"item": "το παράθυρο",
 			"translation": "Окно"
+		},
+		{
+			"item": "η άνοιξη",
+			"translation": "Весна"
+		},
+		{
+			"item": "το καλοκαίρι",
+			"translation": "Лето"
+		},
+		{
+			"item": "το φθινόπωρο",
+			"translation": "Осень"
+		},
+		{
+			"item": "ο χειμώνας",
+			"translation": "Зима"
+		},
+		{
+			"item": "τώρα",
+			"translation": "сейчас"
+		},
+		{
+			"item": "σήμερα",
+			"translation": "сегодня"
+		},
+		{
+			"item": "αύριο",
+			"translation": "завтра"
+		},
+		{
+			"item": "χθες",
+			"translation": "вчера"
+		},
+		{
+			"item": "προχθές",
+			"translation": "позавчера"
+		},
+		{
+			"item": "το πρωϊ",
+			"translation": "утро"
+		},
+		{
+			"item": "το βράδυ",
+			"translation": "вечер"
+		},
+		{
+			"item": "Τι ώρα είναι τώρα;",
+			"translation": "Который час?"
+		},
+		{
+			"item": "τα ρούχα",
+			"translation": "одежда"
+		},
+		{
+			"item": "το παντελόνι",
+			"translation": "брюки"
+		},
+		{
+			"item": "το τζίν",
+			"translation": "джинсы"
+		},
+		{
+			"item": "το κασκέτο",
+			"translation": "кепка"
+		},
+		{
+			"item": "το καλσόν",
+			"translation": "колготки"
+		},
+		{
+			"item": "το μαγιό",
+			"translation": "купальник"
+		},
+		{
+			"item": "το μπουφάν",
+			"translation": "куртка"
+		},
+		{
+			"item": "το φανέλα",
+			"translation": "майка"
+		},
+		{
+			"item": "οι κάλτσες",
+			"translation": "носки"
+		},
+		{
+			"item": "τα γάντια",
+			"translation": "перчатки"
+		},
+		{
+			"item": "το παλτό",
+			"translation": "пальто"
+		},
+		{
+			"item": "το φόρεμα",
+			"translation": "платье"
+		},
+		{
+			"item": "η ζώνη",
+			"translation": "пояс"
+		},
+		{
+			"item": "το πουκάμισο",
+			"translation": "рубашка"
+		},
+		{
+			"item": "το μανίκι",
+			"translation": "рукав"
+		},
+		{
+			"item": "με κοντά μανίκια",
+			"translation": "с длинными рукавами"
+		},
+		{
+			"item": "με μακρυά μανίκια",
+			"translation": "с короткими рукавами"
+		},
+		{
+			"item": "το φανελάκι",
+			"translation": "футболка"
+		},
+		{
+			"item": "το κασκόλ",
+			"translation": "шарф"
+		},
+		{
+			"item": "το καπέλο",
+			"translation": "шляпа"
+		},
+		{
+			"item": "το σορτς",
+			"translation": "шорты"
+		},
+		{
+			"item": "η φούστα",
+			"translation": "юбка"
+		},
+		{
+			"item": "τα παπούτσια",
+			"translation": "обувь"
+		},
+		{
+			"item": "τα πέδιλα",
+			"translation": "босоножки"
+		},
+		{
+			"item": "τα σπορ παπούτσια",
+			"translation": "кроссовки"
+		},
+		{
+			"item": "οι μπότες",
+			"translation": "сапоги"
 		}
 	],
 	"english": [
@@ -387,6 +688,130 @@ var vocabulary = {
 		{
 			"item": "brazen",
 			"translation": "низкий, бесстыдный"
+		},
+		{
+			"item": "account for",
+			"translation": "вычислять"
+		},
+		{
+			"item": "act as",
+			"translation": "работать в качестве; выполнять, чьи-л. функции, замещать кого-л."
+		},
+		{
+			"item": "act for",
+			"translation": "выполнять функции другого лица; представлять кого-л."
+		},
+		{
+			"item": "act on",
+			"translation": "действовать так, как предпологается"
+		},
+		{
+			"item": "act out",
+			"translation": "выражать эмоции действием, вести себя импульсивно."
+		},
+		{
+			"item": "act up",
+			"translation": "барахлить, плохо работать (о машинах);"
+		},
+		{
+			"item": "add to",
+			"translation": "увеличивать, усиливать (что-л.)"
+		},
+		{
+			"item": "add up",
+			"translation": "соответствовать, согласоваться; "
+		},
+		{
+			"item": "adhere to",
+			"translation": "ридерживаться, следовать (закона, правил или договора, чему-л.)"
+		},
+		{
+			"item": "advise against",
+			"translation": "советовать не делать (чего-л.)"
+		},
+		{
+			"item": "advise of",
+			"translation": "сообщать о (чем-л.)"
+		},
+		{
+			"item": "agree with",
+			"translation": "соответствовать (чему-л.); разг. быть полезным"
+		},
+		{
+			"item": "aim at",
+			"translation": "стремиться"
+		},
+		{
+			"item": "allow for",
+			"translation": "вводить поправку на что-л."
+		},
+		{
+			"item": "allude to",
+			"translation": "ссылаться"
+		},
+		{
+			"item": "amount to",
+			"translation": "достигать, составлять; равняться, быть эквивалентным"
+		},
+		{
+			"item": "answer back",
+			"translation": "дерзить, грубить (в ответ)"
+		},
+		{
+			"item": "answer for",
+			"translation": "нести ответственность"
+		},
+		{
+			"item": "appeal to",
+			"translation": "ссылаться"
+		},
+		{
+			"item": "apply to",
+			"translation": "использовать, применять"
+		},
+		{
+			"item": "arrive at",
+			"translation": "достичь результата"
+		},
+		{
+			"item": "ask after",
+			"translation": "осведомляться о здоровье"
+		},
+		{
+			"item": "ask for",
+			"translation": "спрашивать, хотеть видеть; просить к телефону (кого-л.)"
+		},
+		{
+			"item": "ask out",
+			"translation": "приглашать на свидание"
+		},
+		{
+			"item": "ask over",
+			"translation": "приглашать в гости"
+		},
+		{
+			"item": "associate with",
+			"translation": "общаться"
+		},
+		{
+			"item": "attach to",
+			"translation": "присоединяться; придавать значение (чему-л.)"
+		},
+		{
+			"item": "attend to",
+			"translation": "заботиться (о чем/ком-л.);"
+		},
+		{
+			"item": "attribute to",
+			"translation": "объяснять (чем-л.)"
+		},
+		{
+			"item": "auction off",
+			"translation": "торг. продавать с аукциона"
+		},
+		{
+			"item": "average out at",
+			"translation": "в среднем"
 		}
 	]
 };
